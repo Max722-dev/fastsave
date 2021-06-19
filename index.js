9afacf38a50e40eda28df2eee64b7c168ad5dd25
Max722-dev <84333740+Max722-dev@users.noreply.github.com>
2021-06-19 22:43:14 +0300

    Added new Classes to index.js

--- index.js
+++ index.js
@@ -1,9 +1,148 @@
 const database = require("./DB/database.json")
-const fs = require("fs")
+
+async class Objects {
+    constructor(Options = { Static: { Name: String, Value: Number } }) {
+        this.Options = Options
+
+        if (this.Options.Static.Value === 1) {
+            
+        } else if (this.Options.Static.Value === 0) {
+            
+        } else {
+            throw new Error("Cannot set the static value to: " + this.Options.Static.Value)
+        }
+    }
+
+    /**
+    * Name: Gets the Name of the Static
+    * Value: Changes the value of the static
+    */
+
+    ChangeStaticValue = (Name = String, Value = Number) => {
+        if (this.Options.Static.Name === Name) {
+            if (Value === String) {
+                throw new Error("Cannot set the static value a string")
+            } else {
+                if (Value === 1) {
+                    this.Options.Static.Value = 1
+                    console.log("Sat " + this.Options.Static.Name + " Value to: " + Value + " and the static Value is now sat as: " + this.Options.Static.Value)
+                    return
+                } else if (Value === 0) {
+                    this.Options.Static.Value = 0
+                    console.log("Sat " + this.Options.Static.Name + " Value to: " + Value + " and the static Value is now sat as: " + this.Options.Static.Value)
+                    return
+                } else {
+                    throw new Error("Cannot set the value of the static to: " + Value)
+                }
+            }
+        } else {
+            throw new Error("Cannot Find Static With name of: " + Name)
+        }
+    }
+
+    /**
+    * Name: Gets the Static name
+    * Function: Runs the Function like a simple function
+    */
+
+    RunStatic = (Name = String, StaticFunction = Function) => {
+        try {
+            if (this.Options.Static.Name === Name) {
+                console.log("Running Static Function with name of: " + Name)
+                StaticFunction()
+                console.log("Static Function is now Running!")
+            } else {
+                throw new Error("Cannot Find Static with name of: " + Name)
+            }
+        } catch (error) {
+            throw new Error("There was an error while running the StaticFunction: " + error)
+        }
+    }
+}
+
+async class SimpleSaving {
+    constructor() {}
+
+    /**
+    * Key: Saves a Key into the data base
+    * Value: adds a value to the key if the key does not have any value
+    */
+
+    Save = (Key, Value) => {
+        try {
+            if (!database[Key]) {
+                database[Key] = {
+                    value: Value
+                }
+            } else {
+                database[Key] = {
+                    value: Value
+                }
+            }
+        } catch (error) {
+            throw new Error("Cannot Save Key: " + error)
+        }
+    }
+
+    /**
+    * Key: finds the key
+    * if the key is not listed in the dataBase it will send a message in the console saying ```Error: cannot find test```
+    */
+
+    find = (Key) => {
+        try {
+            if (database[Key]) {
+                return database[Key]
+            } else {
+                return console.error("Error: cannot find " + key)
+            }
+        } catch (error) {
+            throw new Error("Cannot get Key: " + error)
+        }
+    }
+
+    /**
+    * Key: Adds a Key if the key not found
+    * Value: adds a Value if there is a key not found or found
+    */
+
+    fetch = (Key, Value) => {
+        try {
+            const KeyName = await database[Key]
+            if (!KeyName) {
+                if (Value) {
+                    database[Key] = {
+                        value: Value
+                    }
+                    return database[Key]
+                } else if (!Value) {
+                    database[Key] = {
+                        value: null
+                    }
+                    return database[Key]
+                }
+            } else if (KeyName) {
+                return KeyName
+            }
+            return
+        } catch (error) {
+            throw new Error("Cannot save or get, Error: " + error)
+        }
+    }
+}
+
+    /**
+    * Key: Saves a Key into the data base
+    * Value: adds a value to the key if the key does not have any value
+    */
 
 module.exports.set = async (Key, Value) => {
     try {
         if (!database[Key]) {
+            database[key] = {
+                value: Value
+            }
+        } else {
             database[key] = {
                 value: Value
             }
@@ -15,9 +154,18 @@ module.exports.set = async (Key, Value) => {
     }
 }
 
+    /**
+    * Key: Saves a Key into the data base
+    * Value: adds a value to the key if the key does not have any value
+    */
+
 module.exports.setID = async (ID, Value) => {
     try {
         if (!database[ID]) {
+            database[ID] = {
+                value: Value
+            }
+        } else  {
             database[ID] = {
                 value: Value
             }
@@ -29,6 +177,11 @@ module.exports.setID = async (ID, Value) => {
     }
 }
 
+    /**
+    * Key: finds the key
+    * if the key is not listed in the dataBase it will send a message in the console saying ```Error: cannot find test```
+    */
+
 module.exports.get = async (Key) => {
     try {
         const KeyName = await database[Key]
@@ -39,6 +192,11 @@ module.exports.get = async (Key) => {
     }
 }
 
+    /**
+    * Key: finds the key
+    * if the key is not listed in the dataBase it will send a message in the console saying ```Error: cannot find test```
+    */
+
 module.exports.getID = async (ID) => {
     try {
         const IDName = await database[ID]
@@ -48,6 +206,11 @@ module.exports.getID = async (ID) => {
         console.error(error)
     }
 }
+
+    /**
+    * Key: Adds a Key if the key not found
+    * Value: adds a Value if there is a key not found or found
+    */
 
 module.exports.fetch = async (Key, Value) => {
     try {
@@ -73,6 +236,11 @@ module.exports.fetch = async (Key, Value) => {
         console.error(error)
     }
 }
+
+    /**
+    * Key: Adds a Key if the key not found
+    * Value: adds a Value if there is a key not found or found
+    */
 
 module.exports.fetchID = async (ID, Value) => {
     try {
@@ -109,5 +277,5 @@ module.exports.Delete = async (Key) => {
     }
 }
 
-module.exports.systemMath = Math
-module.exports.fs = this.fs
+module.exports.Objects = Objects
+module.exports.SimpleSaving = SimpleSaving
