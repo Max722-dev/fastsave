de73f0fe9a4314c166ee77dc38b18328e2f85f0a
Max722-dev <84333740+Max722-dev@users.noreply.github.com>
2021-06-25 17:50:42 +0300

    Changed Files

--- testing/test.js
+++
@@ -1,5 +0,0 @@
-const index = require("../index")
-const FastSaveSimple = new index.SimpleSaving()
-const Objects = new index.Objects({ Static: { Name: "test", Value: "test123"}})
-
-Objects.RunStatic()
